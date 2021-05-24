1d677ea7d2ceace1edee62ed7faa17d981240804
Job Vink <jobvink@MacBook-Pro-van-Job.local>
2021-05-24 10:37:59 +0200

    added dev notebook

--- zf_unet_224_model.py
+++ zf_unet_224_model.py
@@ -46,6 +46,7 @@ def jacard_coef_loss(y_true, y_pred):
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
+
 def dice_loss(y_true, y_pred):
     smooth = 1.
     y_true_f = K.flatten(y_true)
@@ -54,9 +55,9 @@ def dice_loss(y_true, y_pred):
     score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
     return 1. - score
 
+
 def bce_dice_loss(y_true, y_pred):
     return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
-
 
 
 def double_conv_layer(x, size, dropout=0.0, batch_norm=True):
